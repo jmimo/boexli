db0fb9e752dbcc57d132eec0d21bd82d3028097b
Michael Mimo Moratti <mimo@mimo.ch>
2015-11-01 17:34:49 +0100

    added python init.d details

---
+++ initd/sigterm_handler.py
@@ -0,0 +1,7 @@
+def sigterm_handler(_signo, _stack_frame):
+    "When sysvinit sends the TERM signal, cleanup before exiting."
+    print("[" + get_now() + "] received signal {}, exiting...".format(_signo))
+    cleanup_pins()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, sigterm_handler)
