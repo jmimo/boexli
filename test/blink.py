fbb5cb23e726b586149b90142ce41f9f82d39079
Michael Mimo Moratti <mimo@mimo.ch>
2015-11-02 16:43:37 +0000

    added green, orange, red blink

--- test/blink.py
+++ test/blink.py
@@ -3,30 +3,47 @@
 import RPi.GPIO as GPIO
 import time
 
+GREEN=7
+RED=11
+
 def main():
     prepare()
     state = False 
+    gpio = GREEN
+    toggle = False
     while True:
         if state:
-            off()
+            off(gpio)
             state = False
             time.sleep(1)
         else:
-            on()
+            on(gpio)
             time.sleep(1)
             state = True
+            if toggle:
+                toggle = False
+                gpio = RED
+            else:
+                toggle = True
+                gpio = GREEN
     GPIO.cleanup()
 
 def prepare():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(11, GPIO.OUT)
 
-def on():
+def on_green():
     GPIO.output(7,True)
 
-def off():
+def off_green():
     GPIO.output(7,False)
 
+def on(gpio):
+    GPIO.output(gpio,True)
+
+def off(gpio):
+    GPIO.output(gpio,False)
 
 if __name__ == '__main__':
     main()
