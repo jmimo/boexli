dcae049654c48d22368d08f229c30fdb6df7c0d2
Michael Mimo Moratti <mimo@mimo.ch>
2015-11-09 16:02:48 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='boexli',
+    version='1.0.0',
+    packages=['daemon'],
+    url='',
+    license='',
+    author='Michael Mimo Moratti',
+    author_email='mimo@mimo.ch',
+    description=''
+)
